e1d13092e2c7153284778d2e67e6148382bcaa19
Josh Pordon <josh.pordon@gmail.com>
2015-01-17 10:23:18 -0600

    Added OI dumb methods and tank drive command

---
+++ UPS2015/src/org/usfirst/frc/team1675/robot/commands/TankDriveWithJoysticks.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team1675.robot.commands;
+
+
+import org.usfirst.frc.team1675.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Controls the drivetrain in a Tank-Drive style, with each joystick controlling 1 side.
+ */
+public class TankDriveWithJoysticks extends Command {
+
+    public TankDriveWithJoysticks() {
+    	requires(Robot.drivetrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	double leftStick = 0.0;
+    	//Add values later
+    	double rightStick = 0.1;
+    	
+    	Robot.drivetrain.setBackLeftSpeed(leftStick);
+    	Robot.drivetrain.setBackRightSpeed(rightStick);
+    	Robot.drivetrain.setFrontLeftSpeed(leftStick);
+    	Robot.drivetrain.setFrontRightSpeed(rightStick);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
