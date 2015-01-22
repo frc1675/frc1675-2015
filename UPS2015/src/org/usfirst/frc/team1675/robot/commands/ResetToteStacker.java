3e468c7139022bf305d97666ce29619ed48f3718
hakiobo <team1675@UPS>
2015-01-21 19:05:33 -0600

    added DropTote GoDownOneTote and ResetToteStacker commands with basic skeletons.

---
+++ UPS2015/src/org/usfirst/frc/team1675/robot/commands/ResetToteStacker.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team1675.robot.commands;
+
+import org.usfirst.frc.team1675.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ResetToteStacker extends Command {
+
+    public ResetToteStacker() {
+    	requires(Robot.toteStacker);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.toteStacker.goDown();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+        //goes until the motors have reached the bottom area. need encoder to code it
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.toteStacker.setMotors(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
