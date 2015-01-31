07da93371f405a9b94c2eaef2430dd202a797428
Tstano <la_boheme2@sbcglobal.net>
2015-01-30 19:15:06 -0600

    These PID constants are pretty good.  Sometimes it reverses when stopping.  There seems to be a little noise in the signal.  But its overall good.  The half thing was solved by I.

--- UPS2015/src/org/usfirst/frc/team1675/robot/RobotMap.java
+++ UPS2015/src/org/usfirst/frc/team1675/robot/RobotMap.java
@@ -33,36 +33,36 @@ public class RobotMap {
 
 	}
 	public static class DriveEncoders {
-		public static final double DRIVE_MAX_SPEED = 20;  // feet per second of slowest wheel	
+		public static final double DRIVE_MAX_SPEED = 10;  // feet per second of slowest wheel	
 		public static final double DRIVE_WHEEL_DIAMETER = 6;
 		public static class BackRightPIDDefaults{
 			public static final double P = .07;
-			public static final double I = 0;
-			public static final double D = 0;
+			public static final double I = .03;
+			public static final double D = .001;
 			public static final double F = 0;
 		}
 		public static class BackLeftPIDDefaults{
 			public static final double P = .07;
-			public static final double I = 0;
-			public static final double D = 0;
+			public static final double I = .03;
+			public static final double D = .001;
 			public static final double F = 0;
 		}
 		public static class FrontRightPIDDefaults {
 			public static final double P = .07;
-			public static final double I = 0;
-			public static final double D = 0;
+			public static final double I = .03;
+			public static final double D = .001;
 			public static final double F = 0;
 		}
 		public static class FrontLeftPIDDefaults  {
 			public static final double P = .07;
-			public static final double I = 0;
-			public static final double D = 0;
+			public static final double I = .03;
+			public static final double D = .001;
 			public static final double F = 0;
 		}
 		public static class AllThePIDs	{
 			public static final double P = .07;
-			public static final double I = 0;
-			public static final double D = 0;
+			public static final double I = .03;
+			public static final double D = .001;
 			public static final double F = 0;
 		}
 	}
