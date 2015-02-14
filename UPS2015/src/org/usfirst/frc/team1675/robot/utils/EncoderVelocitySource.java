f3f27de46dc7bf1d30ad72d49c65909a8bdc6017
michael <windndust@gmail.com>
2015-02-13 23:19:47 -0600

    Updated EncoderVelocitySource.java to have a SmartDashboard printout and to intake a name for readability.

--- UPS2015/src/org/usfirst/frc/team1675/robot/utils/EncoderVelocitySource.java
+++ UPS2015/src/org/usfirst/frc/team1675/robot/utils/EncoderVelocitySource.java
@@ -5,19 +5,34 @@ import org.usfirst.frc.team1675.robot.RobotMap.DriveEncoders;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 /* 
  * This class is used in PIDSpeedControllerForVelocity.  It makes it so the pidGet gets rate
  * which is more analogous to speed than the unknown thing it gets otherwise.  
 */
 public class EncoderVelocitySource implements PIDSource{
 	Encoder encoder;
+	String name;
+	
 	EncoderVelocitySource(Encoder encoder, int ticksPerRevolution){
 		this.encoder = encoder;	
 		this.encoder.setSamplesToAverage(4);
 		this.encoder.setDistancePerPulse(((RobotMap.DriveEncoders.DRIVE_WHEEL_DIAMETER * Math.PI)/12)/ (ticksPerRevolution));
+		this.name = "No name set";
 	}
+	
+	
+	EncoderVelocitySource(Encoder encoder, int ticksPerRevolution, String name){
+		this.encoder = encoder;	
+		this.encoder.setSamplesToAverage(4);
+		this.encoder.setDistancePerPulse(((RobotMap.DriveEncoders.DRIVE_WHEEL_DIAMETER * Math.PI)/12)/ (ticksPerRevolution));
+		this.name = name;
+	}
+	
 	public double pidGet() {
-		return encoder.getRate();
+		double rate = encoder.getRate(); 
+		SmartDashboard.putNumber("Encoder: "+name, rate);
+		return rate;
 	}
 	public double getRate(){
 		return encoder.getRate();
